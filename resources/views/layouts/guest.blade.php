7aefa15c50a8c5e12859e33514ef0a31b4a43e36
Danilo Quispe <dql@daniloquispe.dev>
2024-07-04 21:16:43 -0500

    Main layout storefront template for guest users

---
+++ resources/views/layouts/guest.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<title>Laravel</title>
+	<link href="{{ asset('favicon.png') }}" rel="icon" />
+	@vite('resources/css/app.css')
+</head>
+<body>
+<header class="site-header">
+	<x-marquee />
+</header>
+<main class="main-content">@yield('content')</main>
+<footer class="site-footer">Site footer</footer>
+@vite('resources/js/app.js')
+@yield('js-footer')
+</body>
+</html>
