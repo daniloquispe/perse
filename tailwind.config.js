152955f024e89b86b170e55c8c348b0599e0d629
Danilo Quispe <dql@daniloquispe.dev>
2024-07-04 21:05:35 -0500

    Add logo-related color palette to Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: [
+		"./resources/**/*.blade.php",
+		"./resources/**/*.js",
+		"./resources/**/*.vue",
+	],
+	theme: {
+		extend: {
+			colors: {
+				palette: {
+					orange: '#ff8300',
+					pink: '#fcf2f5',
+					purple: '#AD60BF',
+					yellow: '#ffc629',
+				}
+			}
+		},
+	},
+	plugins: [],
 }
